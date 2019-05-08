a673864adfd6d8a1661b36852507ca019f8c2141
Nhan Cao <nhancv92@gmail.com>
2019-05-08 17:34:48 +0700

    update

--- app/src/main/java/com/nhancv/denoise/KalmanFilter.java
+++ app/src/main/java/com/nhancv/denoise/KalmanFilter.java
@@ -35,6 +35,51 @@ package com.nhancv.denoise;
  * that tend to be more accurate than those based on a single measurement alone, by estimating a joint probability
  * distribution over the variables for each timeframe. The filter is named after Rudolf E. Kálmán, one of the primary
  * developers of its theory.
+ *
+ * DISCRETE KALMAN FILTER
+ * time equation
+ *
+ * {x_k}^{-} = A * x_{k - 1} + B * u_{k - 1}
+ * {P_k}^{-} = A * P_{k - 1} * A^{T} + Q
+ *
+ * measurement equation
+ *
+ * K_k = {P_k}^{-} * H^{T} ( H * {P_k}^{-} * H^T + R )^{-1}
+ * x_k = {x_k}^{-} + K_k ( z_k - H * {x_k}^{-} )
+ * P_k = (I - K_k * H) * {P_k}^{-}
+ *
+ * P_k  is the estimate error.
+ * K_k  is the Kalman Gain.
+ * R  is the sensor noise
+ * Q  is the process noise
+ * x_k  is the filtered value
+ * z_k  is the measurement
+ * A  is the difference equation that relates the state at the previous time step k – 1
+ * to the state at the current step k (in the absence of either a driving function or process noise). It’s “the system matrix”
+ * B  relates the optional control input u ∈ R to the state x. It’s “the control matrix”
+ * H  relates the state to the measurement zk. It’s the matrix that define the position of the tracked system depending on the measurement.
+ *
+ * In my case, it’s pretty easy :
+ *
+ * I have only one dimension since I’m only interested in filtering the value given by a single laser ray.
+ * The matrix A is thus equal to A = [1].
+ * I have no information about the driving forces. So B can directly be sent to 0 since u is also going to be 0.
+ * H is the same as A since the system is the same as the observation. To see what is the utility of H look at this
+ * http://people.mech.kuleuven.be/~tdelaet/bfl_doc/getting_started_guide/node11.html.
+ *
+ * So the sort kalman equation:
+ * time equation
+ *
+ * {x_k}^{-} = x_{k - 1}
+ * {P_k}^{-} = P_{k - 1} + Q
+ *
+ * measurement equation
+ *
+ * K_k = {P_k}^{-} ( {P_k}^{-} + R )^{-1}
+ * x_k = {x_k}^{-} + K_k ( z_k - {x_k}^{-} )
+ * P_k = (I - K_k) * {P_k}^{-}
+ *
+ * Ref: https://malcolmmielle.wordpress.com/2015/04/29/kalman-filter/
  */
 public class KalmanFilter {
     float _err_measure;
